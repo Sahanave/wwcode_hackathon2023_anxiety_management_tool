43598a82fe55d0f82dc2e1c1df8f4a133915ad32
Sahana Venkatesh <sahanavenkatesh@sahanas-mbp-2.home>
2023-10-22 13:05:59 +0100

    add previous-session-connection

--- setup/database.py
+++ setup/database.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
+from datetime import date
+
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -27,6 +29,58 @@ def create_table(conn, create_table_sql):
         c.execute(create_table_sql)
     except Error as e:
         print(e)
+
+def get_patients_previous_sessions(name):
+
+    conn = sqlite3.connect('aitool.db')
+    sql = f"""SELECT 
+        patients.id AS patient_id,
+        patients.name AS patient_name,
+        patients.age,
+        patients.city,
+        patients.languages,
+        patients.academics,
+        patients.family,
+        sessions.id AS session_id,
+        sessions.session_date,
+        sessions.notes,
+        sessions.learning,
+        sessions.availability_time,
+        sessions.budget
+    FROM 
+        patients
+    INNER JOIN 
+        sessions ON patients.id = sessions.patient_id
+    ORDER BY
+        sessions.session_date
+    WHERE 
+        patients.name = {name}; 
+    """
+    dataframe = pd.read_sql(sql,conn)
+    return dataframe
+
+def insert_into_session(session_info):
+    from backend.therapist_dashboard import get_user_entry
+    conn = sqlite3.connect('aitool.db')
+    c = conn.cursor()
+
+    # Insert a new event with a date
+    patient_name = session_info['patient_name']
+    user_demo_data = get_user_entry(patient_name)
+    patient_id = user_demo_data['patient_id']
+    today = date.today()
+    session_date = str(today)
+    notes = session_info['insight']
+    learning = session_info['learning']
+    availability_time = session_info['availability_time']
+    weekly_budget = session_info['weekly_budget']
+    
+    c.execute("""
+            INSERT INTO sessions (patient_name, patient_id, session_date, notes, learning, availability_time,  budget) 
+            VALUES (?, ?, ?, ?, ?, ?) """,(patient_name, patient_id, session_date, notes, learning, availability_time,weekly_budget))
+
+
+    conn.commit()
 
 def insert_into_patients(demo_info):
     conn = sqlite3.connect('aitool.db')
@@ -75,7 +129,6 @@ def main():
                                     patient_id INTEGER,
                                     session_date TEXT,
                                     notes TEXT,
-                                    previous JSON, 
                                     learning TEXT,
                                     availability_time INTEGER,
                                     budget INTERGER,
